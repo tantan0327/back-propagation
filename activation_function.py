402fe87407f2c2825479dab610b0efa861c5798a
Tange <tantantatange@gmail.com>
2020-05-02 00:50:21 +0900

    created Sigmoid function

--- activation_function.py
+++ activation_function.py
@@ -14,3 +14,18 @@ class Relu:
         dx = dout
 
         return dx
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
