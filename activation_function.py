ec0b8baea275643fce4d3f0d8a7341381b1bc035
Tange <tantantatange@gmail.com>
2020-05-06 21:32:33 +0900

    created back propagation network

--- activation_function.py
+++ activation_function.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Relu:
     def __init__(self):
         self.mask = None
@@ -29,3 +31,34 @@ class Sigmoid:
     def backward(self, dout):
         dx = dout * (1.0 - self.out) * self.out
         return dx
+
+
+def softmax(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+    pass
+
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
+        
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+
+        return self.loss
+
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+
+        return dx
+        
